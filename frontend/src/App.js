cd965d4536dfe9dc10d13c0a2736e8e746f92a3b
= <zelgainemati13@gmail.com>
2021-02-27 21:43:11 +0100

    data table

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,25 @@ import "./App.css";
 import logo from "./contact-logo.PNG";
 import React, { useState } from "react";
 import * as XLSX from "xlsx";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
 
 export default function App() {
+  const useStyles = makeStyles({
+    table: {
+      minWidth: 650,
+    },
+  });
+
   const [results, setResults] = useState([]);
 
   const readExcel = (file) => {
+    setResults([]);
     const promise = new Promise((resolve, reject) => {
       const fileReader = new FileReader();
       fileReader.readAsArrayBuffer(file);
@@ -32,7 +46,7 @@ export default function App() {
 
     promise.then((d) => {
       setResults(d);
-      console.log(d)
+      console.log(d);
     });
   };
   return (
@@ -69,23 +83,58 @@ export default function App() {
               readExcel(file);
             }}
           />
-
-          <table class="table container">
-            <thead>
-              <tr>
-                <th scope="col">City</th>
-                <th scope="col">Company Name</th>
-              </tr>
-            </thead>
-            <tbody>
-              {results.map((d) => (
-                <tr key={d.City}>
-                  <th>{d.City}</th>
-                  <td>{d.CompanyName}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <TableContainer>
+            <Table size="small" aria-label="a dense table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>
+                    <b>Company Name</b>{" "}
+                  </TableCell>
+                  <TableCell>
+                    <b>Country</b>
+                  </TableCell>
+                  <TableCell>
+                    <b>City</b>
+                  </TableCell>
+                  <TableCell>
+                    <b> Address Line 1</b>
+                  </TableCell>
+                  <TableCell>
+                    <b>Longitude</b>
+                  </TableCell>
+                  <TableCell>
+                    <b>Latitude</b>
+                  </TableCell>
+                  <TableCell>
+                    <b>Employees (All Sites)</b>
+                  </TableCell>
+                  <TableCell>
+                    <b>Net Worth (EUR)</b>
+                  </TableCell>
+                  <TableCell>
+                    <b>Phone</b>
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {results.map((d) => (
+                  <TableRow key={d["Company Name"]}>
+                    <TableCell component="th" scope="row">
+                      {d["Company Name"]}
+                    </TableCell>
+                    <TableCell>{d["Country/Region"]}</TableCell>
+                    <TableCell>{d.City}</TableCell>
+                    <TableCell>{d["Address Line 1"]}</TableCell>
+                    <TableCell>{d.Longitude}</TableCell>
+                    <TableCell>{d.Latitude}</TableCell>
+                    <TableCell>{d["Employees (All Sites)"]}</TableCell>
+                    <TableCell>{d["Net Worth (EUR)"]}</TableCell>
+                    <TableCell>{d.Phone}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
         </div>
       </section>
     </div>
