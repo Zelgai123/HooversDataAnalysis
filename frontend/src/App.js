a78ffc31510af0df7aa53c6e555bc24fceb01c0f
Zelgai123 <zelgainemati13@gmail.com>
2021-02-27 17:48:27 +0100

    Form changes and package import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,7 @@
 import "./App.css";
 import logo from "./contact-logo.PNG";
-import dataicon from "./dataicon.PNG";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header>
@@ -21,25 +20,18 @@ function App() {
       </section>
       <br />
       <br />
+      <br />
       <section className="section_introduction">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
-          repudiandae id? Ipsam mollitia laudantium ab distinctio sunt at
-          dolores iste, modi id, repellat sapiente magnam optio cumque quaerat
-          cupiditate rem! Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Magnam at molestiae et, aliquid quasi debitis. Quia tempora non
-          distinctio voluptates adipisci odit inventore aperiam aliquam nemo. Ex
-          quidem id tempora.
-        </p>
-      </section>
-      <section className = "section_upload">
         <p>Upload your data!</p>
         <form action="">
-          <input type="file"/>
+          <input type="file" />
+          <br />
+          <br />
+          <button type="submit" form="form1" value="Submit">
+            Submit
+          </button>
         </form>
       </section>
     </div>
   );
 }
-
-export default App;
