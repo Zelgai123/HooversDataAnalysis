b75dced551aab756f5307d22ffda2a67204882bc
= <zelgainemati13@gmail.com>
2021-02-27 19:01:15 +0100

    upload system

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,51 @@
 import "./App.css";
 import logo from "./contact-logo.PNG";
+import React, { useState } from "react";
+import * as XLSX from "xlsx";
 
 export default function App() {
+  const [results, setResults] = useState([]);
+
+  const readExcel = (file) => {
+    const promise = new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsArrayBuffer(file);
+
+      fileReader.onload = (e) => {
+        const bufferArray = e.target.result;
+
+        const wb = XLSX.read(bufferArray, { type: "buffer" });
+
+        const wsname = wb.SheetNames[0];
+
+        const ws = wb.Sheets[wsname];
+
+        const data = XLSX.utils.sheet_to_json(ws);
+
+        resolve(data);
+      };
+
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+
+    promise.then((d) => {
+      setResults(d);
+      console.log(d)
+    });
+  };
   return (
     <div className="App">
       <header>
-        <img src={logo} />
+        <a href="https://www.contact-software.com/de/" target="_blank">
+          <img src={logo} />
+        </a>
         <p>
-          <a href="https://github.com/Zelgai123/HooversDataAnalysis">
+          <a
+            href="https://github.com/Zelgai123/HooversDataAnalysis"
+            target="_blank"
+          >
             More Info
           </a>
         </p>
@@ -23,14 +61,32 @@ export default function App() {
       <br />
       <section className="section_introduction">
         <p>Upload your data!</p>
-        <form action="">
-          <input type="file" />
-          <br />
-          <br />
-          <button type="submit" form="form1" value="Submit">
-            Submit
-          </button>
-        </form>
+        <div>
+          <input
+            type="file"
+            onChange={(e) => {
+              const file = e.target.files[0];
+              readExcel(file);
+            }}
+          />
+
+          <table class="table container">
+            <thead>
+              <tr>
+                <th scope="col">City</th>
+                <th scope="col">Company Name</th>
+              </tr>
+            </thead>
+            <tbody>
+              {results.map((d) => (
+                <tr key={d.City}>
+                  <th>{d.City}</th>
+                  <td>{d.CompanyName}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </section>
     </div>
   );
